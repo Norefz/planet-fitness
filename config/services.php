9320d8c32f32cd6cab7ea29802475742f906ff9f
norefz <noref.kyoref@gmail.com>
2026-07-16 22:41:13 +0700

    fix services

--- config/services.php
+++ config/services.php
@@ -47,6 +47,7 @@ return [
     'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
     'api_key'    => env('CLOUDINARY_API_KEY'),
     'api_secret' => env('CLOUDINARY_API_SECRET'),
-    'folder'     => 'workout-exercises',
+        'folder'     => 'workout-exercises',
+    ],
 
 ];
